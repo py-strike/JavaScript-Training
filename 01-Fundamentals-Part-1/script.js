3a75b847537eac4ebb43e3ca508e476750b8baa8
Khaled <codepower.help@gmail.com>
2025-03-05 17:36:30 +0100

    convension for writing vars is the camelCase

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,12 @@ let js = "Salem";
 if (js === "Salem") {
     alert("Salem Aliko");
 }
+
+// variables and values
+let firstName = "Kevin";
+console.log(firstName);
+
+// convension for writing vars is the camelCase
+let PI = 3.14;
+let myFisrtJob = "Teacher";
+let job1 = "Programmer";
