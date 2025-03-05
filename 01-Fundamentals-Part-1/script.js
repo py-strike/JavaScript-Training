1a03862e0a34da5f25c7da9667023ec43743b6c0
Khaled <codepower.help@gmail.com>
2025-03-05 18:15:43 +0100

    Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,7 @@
 let js = "Salem";
-if (js === "Salem") {
-    alert("Salem Aliko");
-}
+// if (js === "Salem") {
+//     alert("Salem Aliko");
+// }
 
 // variables and values
 let firstName = "Kevin";
@@ -11,3 +11,16 @@ console.log(firstName);
 let PI = 3.14;
 let myFisrtJob = "Teacher";
 let job1 = "Programmer";
+
+// Data Types
+// Numbers,
+let age = 32;
+// Strings
+let msg = "Hello";
+// Boolean
+let fullAge = true;
+// Undefined
+let children;
+// Null
+// Symbol
+// BigInt
